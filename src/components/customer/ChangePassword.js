93f8c1c8c1340732d54dddf3548c46e217f4abe6
Nguyen Minh Phung <phungnm99@gmail.com>
2021-12-11 14:34:17 +0700

    change password

--- src/components/customer/ChangePassword.js
+++ src/components/customer/ChangePassword.js
@@ -1,14 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useContext } from "react";
 import iconClose from "../../images/iconClose.png";
-import "../../css/customer/changePassword.css"
+import "../../css/customer/changePassword.css";
+import userApi from "../../api/userApi";
+import { AuthContext } from "../../AuthContext";
+import { useHistory } from "react-router-dom";
+import Auth from "../../config/auth";
 
 function ChangePasswordForm(props) {
   //const [isLogin, setIsLogin] = useState(true);
   const [checkPassword, setCheckPass] = useState(true);
+  const history = useHistory();
+  const context = useContext(AuthContext);
 
   const validate = () => {
-    let password = document.getElementById("NewPassword")?.value || '';
-    let rePass = document.getElementById("NewPasswordAgain")?.value || '';
+    let password = document.getElementById("NewPassword")?.value || "";
+    let rePass = document.getElementById("NewPasswordAgain")?.value || "";
     setCheckPass(password === rePass);
   };
 
@@ -16,15 +22,46 @@ function ChangePasswordForm(props) {
     e.preventDefault();
 
     let oldPassword = document.getElementById("OldPassword").value;
-    if(oldPassword === "")
-    alert("Vui lòng nhập mật khẩu cũ !!!");
+    if (oldPassword === "") alert("Vui lòng nhập mật khẩu cũ !");
     else {
       let newPassword = document.getElementById("NewPassword").value;
-      if(newPassword === "" || newPassword.length < 8) {
-        alert("Mật khẩu mới phải hợp lệ!");
+      if (newPassword === "") {
+        alert("Vui lòng nhập mật khẩu mới!");
+      } else {
+        let newPasswordAgain =
+          document.getElementById("NewPasswordAgain").value;
+        if (newPasswordAgain === "") {
+          alert("Vui lòng nhập nhật lại mật khẩu!");
+        } else {
+          if (newPassword !== newPasswordAgain) {
+            alert("Mật khẩu mới và nhập lại mật khẩu phải giống nhau!");
+          } else {
+            let regex =
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
+            if (regex.test(newPassword) === false) {
+              alert(
+                "Mật khẩu mới không hợp lệ! Mật khẩu phải có 8 kí tự trở lên, có ít nhất 1 kí tự thường, 1 kí tự in hoa và 1 kí tự số!"
+              );
+            } else {
+              let formData = new FormData();
+              formData.append("OldPassword", oldPassword);
+              formData.append("NewPassword", newPassword);
+              userApi.changePassword(formData).then((res) => {
+                if (res.code === "401") {
+                  alert("Mật khẩu không chính xác!");
+                } else {
+                  alert("Đổi mật khẩu thành công! Vui lòng đăng nhập lại!");
+                  context.logout();
+                  Auth.logout();
+                  history.push("/login");
+                }
+              });
+            }
+          }
+        }
       }
     }
-  }
+  };
 
   return (
     <div>
@@ -36,58 +73,58 @@ function ChangePasswordForm(props) {
                 alt="icon-close"
                 className="icon-close"
                 src={iconClose}
-                
                 onClick={() => props.onCLose()}
               />
               <div className="changePassword">
-                  <div>
-                    <p>ĐỔI MẬT KHẨU</p>
+                <div>
+                  <p>ĐỔI MẬT KHẨU</p>
+                </div>
+                <div className="js-validation-reminder form-horizontal push-30-t push-50">
+                  <div className="row">
+                    <label>Nhập mật khẩu cũ</label>
+                    <input
+                      className="form-control"
+                      type="Password"
+                      id="OldPassword"
+                      name="OldPassword"
+                      placeholder="Nhập mật khẩu cũ"
+                    />
                   </div>
-                  <div
-                    className="js-validation-reminder form-horizontal push-30-t push-50"
-                  >
-                    <div className="row">
-                      <label>Nhập mật khẩu cũ</label>
-                      <input
-                        className="form-control"
-                        type="Password"
-                        id="OldPassword"
-                        name="OldPassword"
-                        placeholder="Nhập mật khẩu cũ"
-                      />
-                    </div>
-                    <div className="row">
-                      <label>Mật khẩu mới</label>
-                      <input
-                        className="form-control"
-                        type="Password"
-                        id="NewPassword"
-                        name="NewPassword"
-                        placeholder="Nhập mật khẩu mới"
-                        onChange={(e) => validate()}
-                      />
-                    </div>
-                    <div className="row">
-                      <label>Nhập lại mật khẩu mới</label>
-                      <input
-                        className="form-control"
-                        type="Password"
-                        id="NewPasswordAgain"
-                        name="NewPasswordAgain"
-                        placeholder="Nhập lại mật khẩu mới"
-                        onChange={(e) => validate()}
-                      />
-                    
-                      {checkPassword ? null : <p className="messageError">Nhập lại password không trùng khớp.</p>}
-                    </div>
-                    <div className="row">
-                      <button onClick={(e) => handleSubmit(e)}>
-                        <i class="si si-envelope-open pull-right"></i>Đổi mật
-                        khẩu
-                      </button>
-                    </div>
+                  <div className="row">
+                    <label>Mật khẩu mới</label>
+                    <input
+                      className="form-control"
+                      type="Password"
+                      id="NewPassword"
+                      name="NewPassword"
+                      placeholder="Nhập mật khẩu mới"
+                      onChange={(e) => validate()}
+                    />
+                  </div>
+                  <div className="row">
+                    <label>Nhập lại mật khẩu mới</label>
+                    <input
+                      className="form-control"
+                      type="Password"
+                      id="NewPasswordAgain"
+                      name="NewPasswordAgain"
+                      placeholder="Nhập lại mật khẩu mới"
+                      onChange={(e) => validate()}
+                    />
+
+                    {checkPassword ? null : (
+                      <p className="messageError">
+                        Nhập lại password không trùng khớp.
+                      </p>
+                    )}
+                  </div>
+                  <div className="row">
+                    <button onClick={(e) => handleSubmit(e)}>
+                      <i class="si si-envelope-open pull-right"></i>Đổi mật khẩu
+                    </button>
                   </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
