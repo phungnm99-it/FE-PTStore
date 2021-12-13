24d7c38f7087be59b1657bf398bb1d18219a5413
Nguyen Minh Phung <phungnm99@gmail.com>
2021-12-13 21:11:53 +0700

    fix slide image

--- src/components/common/Slide.js
+++ src/components/common/Slide.js
@@ -2,9 +2,6 @@ import React from "react";
 import "../../css/common/Slide.css";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import slide1 from "../../images/slide1.jpg";
-import slide2 from "../../images/slide2.png";
-import slide3 from "../../images/slide3.png";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 function Slide() {
   const history = useHistory();
@@ -32,27 +29,41 @@ function Slide() {
                         Đến với PTStore bạn sẽ có được sản phẩm chất lượng, giá
                         cả hợp lý và dịch vụ bảo hành trọn gói
                       </p>
-                      <button type="button" className="btn btn-default get" onClick={() => history.push("/")}>
+                      <button
+                        type="button"
+                        className="btn btn-default get"
+                        onClick={() => history.push("/")}
+                      >
                         Mua ngay
                       </button>
                     </div>
                   </div>
                   <div className="col-sm-6">
                     <div className="slide-img">
-                      <img className="slide1" src={slide1} alt="" />
+                      <img
+                        className="slide1"
+                        src="http://res.cloudinary.com/dobsh4rbw/image/upload/v1639404655/commom/slide1121321091011.jpg"
+                        alt=""
+                      />
                     </div>
                   </div>
                 </div>
 
                 <div className="item no-padding">
                   <div className="slide-img">
-                    <img src={slide2} alt="" />
+                    <img
+                      src="http://res.cloudinary.com/dobsh4rbw/image/upload/v1639404688/commom/slide2121321091044.png"
+                      alt=""
+                    />
                   </div>
                 </div>
 
                 <div className="item no-padding">
                   <div className="slide-img">
-                    <img src={slide3} alt="" />
+                    <img
+                      src="http://res.cloudinary.com/dobsh4rbw/image/upload/v1639404712/commom/slide3121321091108.png"
+                      alt=""
+                    />
                   </div>
                 </div>
               </Carousel>
