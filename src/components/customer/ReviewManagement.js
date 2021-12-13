495408660ef669f2a39728c888652448db2195e3
Nguyen Minh Phung <phungnm99@gmail.com>
2021-12-13 21:09:08 +0700

    fix no review image

--- src/components/customer/ReviewManagement.js
+++ src/components/customer/ReviewManagement.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "../../css/customer/reviewManagement.css";
 import userApi from "../../api/userApi";
-import noReview from "../../images/noReview.png"
-import "../../css/customer/noOrder.css"
+import "../../css/customer/noOrder.css";
 import { timeFormat } from "../../utils/dateUtils";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 function ReviewManagement() {
@@ -22,28 +21,37 @@ function ReviewManagement() {
 
   return review.length === 0 ? (
     <section className="orderHistory">
-        <div className="body-content">
-          <h1>Đánh giá của bạn</h1>
-          <div className="account-layout">
-            <div className="row equaHeight" data-obj=".col .box-bg-white">
-              <div className="col col-lg">
-                <div className="box-bg-white">
-                  <div className="not-found-list">
-                    <a>
-                      <img src={noReview} alt="not-found-list" />
-                    </a>
-                    <p>Bạn chưa có đánh giá nào</p>
-                    <span>Hãy mua hàng và cho cửa hàng cảm nhận về trải nghiệm của bạn nhé.</span>
-                    <button className="button_direct" onClick={() => history.push("/")}>
-                      Về trang chủ
-                    </button>
-                  </div>
+      <div className="body-content">
+        <h1>Đánh giá của bạn</h1>
+        <div className="account-layout">
+          <div className="row equaHeight" data-obj=".col .box-bg-white">
+            <div className="col col-lg">
+              <div className="box-bg-white">
+                <div className="not-found-list">
+                  <a>
+                    <img
+                      src="http://res.cloudinary.com/dobsh4rbw/image/upload/v1639404561/commom/noReview121321090831.png"
+                      alt="not-found-list"
+                    />
+                  </a>
+                  <p>Bạn chưa có đánh giá nào</p>
+                  <span>
+                    Hãy mua hàng và cho cửa hàng cảm nhận về trải nghiệm của bạn
+                    nhé.
+                  </span>
+                  <button
+                    className="button_direct"
+                    onClick={() => history.push("/")}
+                  >
+                    Về trang chủ
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   ) : (
     <div>
       <section className="reviewHistory">
@@ -73,9 +81,7 @@ function ReviewManagement() {
                           return (
                             <tr key={item.id}>
                               <td>{item.productName}</td>
-                              <td>
-                                {timeFormat(item.reviewTime)}
-                              </td>
+                              <td>{timeFormat(item.reviewTime)}</td>
                               <td>{item.content}</td>
                             </tr>
                           );
